bc3d6097bb168d7f16a56a6712d0c76cd9c68db6
侯凯召 <3046916186@qq.com>
2025-03-23 18:08:27 +0800

    新增两个地图 轮换

--- wolf/JavaScripts/Tables/Level.ts
+++ wolf/JavaScripts/Tables/Level.ts
@@ -1,5 +1,5 @@
 import { ConfigBase, IElementBase } from "./ConfigBase";
-const EXCELDATA:Array<Array<any>> = [["ID","Name","Remark","BirthName","BirthPoint","ImageGuid","PlayerSpeed"],["","","","","","",""],[10000,"水上乐园","大厅","PlayStart_Hall","2C4F453B",null,"350"],[10001,"水上乐园","夜色酒吧","PlayStart_Inside_001","6A4B9B76","292240","350"],[10002,"水上乐园","樱花咖啡馆","PlayStart_Inside_002","6A4B9B76","292241","350"],[10003,"水上乐园","泳池派对","PlayStart_Inside_003","6A4B9B76","292252","430"]];
+const EXCELDATA:Array<Array<any>> = [["ID","Name","Remark","BirthName","BirthPoint","ImageGuid","PlayerSpeed"],["","Language","","","","",""],[10000,"Level_Name_10000","大厅","PlayStart_Hall","2C4F453B",null,"350"],[10001,"Level_Name_10001","邮轮之旅","PlayStart_Inside_001","6A4B9B76","292241","350"],[10002,"Level_Name_10002","樱花咖啡馆","PlayStart_Inside_002","10DBE474","292222","350"],[10003,"Level_Name_10003","运输船","PlayStart_Inside_003","05A7AE9D","563372","430"]];
 export interface ILevelElement extends IElementBase{
  	/**序号*/
 	ID:number
